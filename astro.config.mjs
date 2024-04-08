53526d7b78c59e13e447f5f08a03381f7910ea19
Gerardo Gallardo Rodríguez <gherardgr2@gmail.com>
2024-04-08 13:53:43 -0400

    Revert "fix astro.conifg file"
    This reverts commit 7358ac294e8b146ba12b6989183417961bf4adf6.

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,6 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  site: 'https://GerardoEgR.github.io',
+  site: 'https://github.com/GerardoEgR',
+  base: 'https://pageEffeta.pages.github.io/',
 });
