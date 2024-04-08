ec8d9feb99c39381a68d75531006a01a52d62f2a
Gerardo Gallardo Rodríguez <gherardgr2@gmail.com>
2024-04-08 01:44:56 -0400

    Config deploy.yml file

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind()],
+  site: 'https://github.com/GerardoEgR',
+  base: 'https://effeta.pages.github.io/',
 });
